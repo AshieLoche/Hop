eb8706262abb053c4c490503741bbbc4d70a32af
Ashie Loche <134357122+AshieLoche@users.noreply.github.com>
2023-06-20 15:02:36 +0800

    Tile and Animation

--- Assets/Dungeon Knight/Scripts/PlayerMovement.cs
+++ Assets/Dungeon Knight/Scripts/PlayerMovement.cs
@@ -13,6 +13,15 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 movementInput;
     //Animator access our animator to play animation
     private Animator anim;
+    private enum SpriteOrientation
+    {
+        ORIENT_LEFT,
+        ORIENT_FORWARD,
+        ORIENT_RIGHT,
+        ORIENT_BACKWARD
+    }
+    private SpriteOrientation orientation = SpriteOrientation.ORIENT_FORWARD;
+    private bool movementChecker = true;
 
     // Start is called before the first frame update
     void Start()
@@ -25,38 +34,69 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)) 
+        if (/*movementChecker &&*/ (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)))
         {
+            resetAnimation();
             anim.SetTrigger("backward");
-        } 
-        else if (Input.GetKeyDown(KeyCode.A))
+            orientation = SpriteOrientation.ORIENT_BACKWARD;
+        }
+        if (/*movementChecker &&*/ (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)))
         {
+            resetAnimation();
             anim.SetTrigger("left");
+            orientation = SpriteOrientation.ORIENT_LEFT;
         }
-        else if (Input.GetKeyDown(KeyCode.S))
+        if (/*movementChecker &&*/ (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)))
         {
+            resetAnimation();
             anim.SetTrigger("forward");
+            orientation = SpriteOrientation.ORIENT_FORWARD;
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+        if (/*movementChecker &&*/ (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))) 
         {
+            resetAnimation();
             anim.SetTrigger("right");
+            orientation = SpriteOrientation.ORIENT_RIGHT;
         }
-        else if (Input.GetKeyUp(KeyCode.W))
+        if (/*!movementChecker &&*/ (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)))
         {
             anim.SetTrigger("backwardPause");
+            //movementChecker = true;
         }
-        else if (Input.GetKeyUp(KeyCode.A))
+        if (/*!movementChecker &&*/ (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow)))
         {
             anim.SetTrigger("leftPause");
+            //movementChecker = true;
         }
-        else if (Input.GetKeyUp(KeyCode.S))
+        if (/*!movementChecker &&*/ (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow)))
         {
             anim.SetTrigger("forwardPause");
+            //movementChecker = true;
         }
-        else if (Input.GetKeyUp(KeyCode.D))
+        if (/*!movementChecker &&*/ (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow)))
         {
             anim.SetTrigger("rightPause");
+            //movementChecker = true;
         }
+        if (Input.GetMouseButtonDown(0) && orientation == SpriteOrientation.ORIENT_FORWARD) 
+        {
+            resetAnimation();
+            anim.SetTrigger("forwardStab");
+        }
+        if (Input.GetMouseButtonUp(0) && orientation == SpriteOrientation.ORIENT_FORWARD)
+        {
+            anim.SetTrigger("forwardPause");
+            //movementChecker = true;
+        }
+    }
+
+    private void resetAnimation()
+    {
+        anim.ResetTrigger("forwardPause");
+        anim.ResetTrigger("rightPause");
+        anim.ResetTrigger("leftPause");
+        anim.ResetTrigger("backwardPause");
+        //movementChecker = false;
     }
 
     //Fixed update for physics calculations
